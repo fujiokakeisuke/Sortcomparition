f298d0b1938d2ac17a3cc0a3027e24527f06c367
unknown <>
2017-03-21 14:17:33 +0900

    add new file

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace SortComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = 40000;
+            int g;
+            Console.OutputEncoding = Encoding.UTF8;
+            bubblesort b = new bubblesort();
+            quicksort q = new quicksort();
+            mergesort m = new mergesort();
+            for (int i = 0; i < t; i++)
+            {
+                System.Random r = new System.Random(i);
+                g = (r.Next(t)+1);
+                b.a[i] = g;
+                q.a[i] = g;
+                m.a[i] = g;
+            }
+            b.sort();
+            q.sort();
+            m.sort();
+            string j = Console.ReadLine();
+            
+        }
+    }
+}
---
+++ bubblesort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+
+namespace SortComparison
+{
+    class bubblesort
+    {
+        public const int t = 40000;
+        public int[] a = new int[t];
+        Stopwatch sw = new Stopwatch();
+        
+        public void sort()
+        {
+            sw.Start();
+            int k;
+            for(int i = 0; i <  t - 1;++i)
+            {
+                for(int j = t -1;i < j; --j)
+                {
+                    if (a[i] > a[j])
+                    {
+                        k = a[i]; a[i] = a[j]; a[j] = k; 
+                    }
+                }
+            }
+            sw.Stop();
+            Console.WriteLine("バブルソートで並べ替えをした配列の百個目までを表示します。");
+            for(int i = 0; i < 100; ++i)
+            {
+                Console.Write("{0}"+" ", a[i]);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("実行時間は{0}ミリ秒です。", sw.ElapsedMilliseconds);
+
+        }
+    }
+}
